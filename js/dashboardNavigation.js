7dd7dd873213c6528a21a4372f9c3a525a3d7982
Miguel Henriquez <mig.henriquezp@gmail.com>
2022-04-05 23:26:14 -0400

    Implementación inicial dashboard sin indexar a homepage

---
+++ js/dashboardNavigation.js
@@ -0,0 +1,21 @@
+
+//Navigation
+
+const btnMenu = document.getElementById('searcher__burger');
+const nav = document.querySelector('.dashboard-navigation');
+const buttons = document.querySelector('.nav');
+btnMenu.addEventListener('click', () => {
+    nav.classList.toggle('dashboard-navigation--show');
+    console.log('clciked');
+});
+
+window.addEventListener("click", w => {
+    // console.log(w.target);
+    if(nav.classList.contains("dashboard-navigation--show") && w.target != document.querySelector(".nav__item")
+    && w.target != document.querySelector(".nav__name")
+    && w.target != document.querySelector(".nav__navigation")
+    && w.target != document.querySelector(".dashboard-navigation")
+    && w.target != document.querySelector("#searcher__burger")) {
+        nav.classList.toggle("dashboard-navigation--show");
+    }
+});
